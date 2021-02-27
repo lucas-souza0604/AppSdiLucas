e9b3911e476fb3cb62ea6abc2c94348af7181235
lucas souza <lucas.dsouza@al.infnet.edu.br>
2021-02-27 20:58:41 -0300

    criacao do proj lucas

---
+++ src/main/java/br/edu/infnet/appsdi/aula01/Teste.java
@@ -0,0 +1,12 @@
+package br.edu.infnet.appsdi.aula01;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		Aluno aluno = new Aluno();
+		aluno.setId(1);
+		aluno.setNome("Lucas");
+		aluno.setEmail("lucas@gmail.com");
+		System.out.println(aluno);
+	}
+}
